6d62bffac6d22cd35c14dd46623b694fc5b02fde
PopovAlex <c76167970@gmail.com>
2021-10-22 16:28:35 +0200

    movie_search.php: объединил циклы

--- movie_search.php
+++ movie_search.php
@@ -14,27 +14,25 @@ if (!is_numeric($userAge) || (int)$userAge < 0)
 
 $recommendedFilms = [];
 
+echo "That's your list of recommended films to you:\n";
+
+$filmIndex = 1;
+
 /** @var TYPE_NAME $movies */
 foreach ($movies as $film)
 {
 	if ($film['age_restriction'] <= (int)$userAge)
 	{
-		$recommendedFilms[] = $film;
+		echo $filmIndex
+			. '. '
+			. $film['title']
+			. ' ('
+			. $film['release_year']
+			. ') '
+			. $film['age_restriction']
+			. '+. Rating - '
+			. $film['rating']
+			. "\n";
+		$filmIndex++;
 	}
-}
-
-echo "That's your list of recommended films to you:\n";
-
-foreach ($recommendedFilms as $filmIndex => $film)
-{
-	echo $filmIndex + 1
-		. '. '
-		. $film['title']
-		. ' ('
-		. $film['release_year']
-		. ') '
-		. $film['age_restriction']
-		. '+. Rating - '
-		. $film['rating']
-		. "\n";
 }
